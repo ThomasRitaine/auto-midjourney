ff860d4ddd6765de79d77c360c37df98449bc5fc
Thomas Ritaine <thomas.ritaine@amaris.com>
2023-09-26 20:12:59 +0200

    feat(Views): :sparkles: Upgrade collection views

--- src/app.ts
+++ src/app.ts
@@ -1,99 +1,127 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import generateAndDownload from './midjourney/generateAndDownload';
-import path from 'path';
+import express from "express";
+import bodyParser from "body-parser";
+import generateAndDownload from "./midjourney/generateAndDownload";
+import path from "path";
 import {
-    createCollection,
-    getCollectionByName,
-    getAllCollections,
-    getCollectionBySlug
-} from './services/prisma-crud/collection';
-import { getImagesByCollectionId } from './services/prisma-crud/image';
-import { Collection, GenerationInfo } from '@prisma/client';
-import { createGenerationInfo } from './services/prisma-crud/generationInfo';
-
+  createCollection,
+  getCollectionByName,
+  getAllCollections,
+  getCollectionBySlug,
+  getNumberImageOfCollection,
+  getFirstImagesOfCollectionId,
+} from "./services/prisma-crud/collection";
+import {
+  deleteImage,
+  getImagesByCollectionId,
+} from "./services/prisma-crud/image";
+import { Collection, GenerationInfo } from "@prisma/client";
+import { createGenerationInfo } from "./services/prisma-crud/generationInfo";
 
 const app = express();
 const port = 3000;
 
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, '..', 'public')));
-app.use('/image', express.static(path.join(__dirname, '..', 'image')));
+app.use(express.static(path.join(__dirname, "..", "public")));
+app.use("/image", express.static(path.join(__dirname, "..", "image")));
 
-app.get('/', (req, res) => {
-    res.render('generate');
+app.get("/", (req, res) => {
+  res.render("generate");
 });
 
-app.post('/generate', async (req, res) => {
+app.post("/generate", async (req, res) => {
+  const prompts = Array.isArray(req.body.prompt)
+    ? req.body.prompt
+    : [req.body.prompt];
+  const repeats = Array.isArray(req.body.repeat)
+    ? req.body.repeat
+    : [req.body.repeat];
+  const collectionsName = Array.isArray(req.body.collection)
+    ? req.body.collection
+    : [req.body.collection];
 
-    const prompts = Array.isArray(req.body.prompt) ? req.body.prompt : [req.body.prompt];
-    const repeats = Array.isArray(req.body.repeat) ? req.body.repeat : [req.body.repeat];
-    const collectionsName = Array.isArray(req.body.collection) ? req.body.collection : [req.body.collection];
+  const collections: Collection[] = [];
 
-    const collections: Collection[] = [];
-
-    for (const collectionName of collectionsName) {
-        let collection = await getCollectionByName(collectionName);
-        if (!collection) {
-            collection = await createCollection(collectionName);
-        }
-        collections.push(collection);
+  for (const collectionName of collectionsName) {
+    let collection = await getCollectionByName(collectionName);
+    if (!collection) {
+      collection = await createCollection(collectionName);
     }
+    collections.push(collection);
+  }
 
-    const generationInfoGroup: GenerationInfo[] = [];
+  const generationInfoGroup: GenerationInfo[] = [];
 
-    for (let index = 0; index < prompts.length; index++) {
-        const prompt = prompts[index];
-        const generationInfo = await createGenerationInfo({
-            prompt,
-            repeat: parseInt(repeats[index]),
-            collection: {
-                connect: {
-                    id: collections[index].id
-                }
-            },
-        });
-        generationInfoGroup.push(generationInfo);
-    }
+  for (let index = 0; index < prompts.length; index++) {
+    const prompt = prompts[index];
+    const generationInfo = await createGenerationInfo({
+      prompt,
+      repeat: parseInt(repeats[index]),
+      collection: {
+        connect: {
+          id: collections[index].id,
+        },
+      },
+    });
+    generationInfoGroup.push(generationInfo);
+  }
 
-    generateAndDownload(generationInfoGroup);
+  generateAndDownload(generationInfoGroup);
 
-    const isCollectionUnique = generationInfoGroup.every(item => item.id === generationInfoGroup[0].id);
+  const isCollectionUnique = generationInfoGroup.every(
+    (item) => item.id === generationInfoGroup[0].id
+  );
 
-    generationInfoGroup.forEach(generationInfo => {
-        console.log(`Generating id : ${generationInfo.id}`);
-    });        
+  generationInfoGroup.forEach((generationInfo) => {
+    console.log(`Generating id : ${generationInfo.id}`);
+  });
 
-    if(generationInfoGroup.length === 1 || isCollectionUnique) {
-        res.redirect(`/collection/${collections[0].slug}`);
-    } else {
-        res.redirect('/collection');
-    }
+  if (generationInfoGroup.length === 1 || isCollectionUnique) {
+    res.redirect(`/collection/${collections[0].slug}`);
+  } else {
+    res.redirect("/collection");
+  }
 });
 
+app.get("/collection", async (req, res) => {
+  const collections = await getAllCollections();
 
-app.get('/collection', async (req, res) => {
-    
-    const collections = await getAllCollections();
+  // Get the number of image in each collection
+  let numberImagesInCollection: { [key: string]: number } = {};
+  for (const collection of collections) {
+    numberImagesInCollection[collection.id] = await getNumberImageOfCollection(
+      collection.id
+    );
+  }
 
-    res.render('collections', { collections });
+  // Get the first image of each collection
+  let firstImageOfCollection: { [key: string]: string | null } = {};
+  for (const collection of collections) {
+    const image = await getFirstImagesOfCollectionId(collection.id);
+    firstImageOfCollection[collection.id] = image ? image.path : null;
+  }
+
+  res.render("collections", {
+    collections,
+    numberImagesInCollection,
+    firstImageOfCollection,
+  });
 });
 
-app.get('/collection/:slug', async (req, res) => {
-    const slug = req.params.slug;
-    const collection = await getCollectionBySlug(slug);
+app.get("/collection/:slug", async (req, res) => {
+  const { slug } = req.params;
+  const collection = await getCollectionBySlug(slug);
 
-    // return 404 if the collection doesn't exist
-    if (!collection) return res.status(404).send('Collection not found');
+  // return 404 if the collection doesn't exist
+  if (!collection) return res.status(404).send("Collection not found");
 
-    const images = await getImagesByCollectionId(collection.id);
+  const images = await getImagesByCollectionId(collection.id);
 
-    res.render('collection', { images, collection: collection.name });
+  res.render("collection", { images, collection: collection.name });
 });
 
 
 app.listen(port, () => {
-    console.log(`Server started on http://localhost:${port}`);
+  console.log(`Server started on http://localhost:${port}`);
 });
--- src/services/prisma-crud/collection.ts
+++ src/services/prisma-crud/collection.ts
@@ -1,43 +1,77 @@
-import { Collection, PrismaClient } from '@prisma/client';
+import { Collection, PrismaClient } from "@prisma/client";
 import { mkdir } from "fs/promises";
-import slugify from '../../util/slugify';
-
+import slugify from "../../util/slugify";
 
 const prisma = new PrismaClient();
 
 export const createCollection = async (name: string): Promise<Collection> => {
-    const slug = slugify(name);
-    const newCollection = await prisma.collection.create({ data: {name, slug} });
-    const folder = `image/${slug}`;
-    await mkdir(folder, { recursive: true });
-    console.log("Collection created", newCollection.name);
-    console.log("Collection slug", newCollection.slug);
-    return newCollection;
+  const slug = slugify(name);
+  const newCollection = await prisma.collection.create({
+    data: { name, slug },
+  });
+  const folder = `image/${slug}`;
+  await mkdir(folder, { recursive: true });
+  console.log("Collection created", newCollection.name);
+  console.log("Collection slug", newCollection.slug);
+  return newCollection;
 };
 
 export const getAllCollections = async (): Promise<Collection[]> => {
-    return await prisma.collection.findMany();
+  return await prisma.collection.findMany();
 };
 
-export const getCollectionByName = async (name: string): Promise<Collection|null> => {
-    return await prisma.collection.findUnique({ where: { name } });
+export const getCollectionByName = async (
+  name: string
+): Promise<Collection | null> => {
+  return await prisma.collection.findUnique({ where: { name } });
 };
 
-export const getCollectionBySlug = async (slug: string): Promise<Collection|null> => {
-    return await prisma.collection.findUnique({ where: { slug } });
+export const getCollectionBySlug = async (
+  slug: string
+): Promise<Collection | null> => {
+  return await prisma.collection.findUnique({ where: { slug } });
 };
 
-export const getCollectionById = async (id: string): Promise<Collection|null> => {
-    return await prisma.collection.findUnique({ where: { id } });
+export const getCollectionById = async (
+  id: string
+): Promise<Collection | null> => {
+  return await prisma.collection.findUnique({ where: { id } });
 };
 
-export const updateCollection = async (id: string, updatedData: any): Promise<Collection> => {
-    return await prisma.collection.update({
-        where: { id },
-        data: updatedData
-    });
+export const getNumberImageOfCollection = async (
+  id: string
+): Promise<number> => {
+  return await prisma.image.count({
+    where: {
+      collectionId: id,
+    },
+  });
+};
+
+export const getFirstImagesOfCollectionId = async (
+  id: string
+): Promise<{ path: string } | null> => {
+  const result = await prisma.image.findFirst({
+    where: {
+      collectionId: id,
+    },
+    select: {
+      path: true,
+    },
+  });
+  return result ? { path: result.path } : null;
+};
+
+export const updateCollection = async (
+  id: string,
+  updatedData: any
+): Promise<Collection> => {
+  return await prisma.collection.update({
+    where: { id },
+    data: updatedData,
+  });
 };
 
 export const deleteCollection = async (id: string) => {
-    return await prisma.collection.delete({ where: { id } });
+  return await prisma.collection.delete({ where: { id } });
 };
