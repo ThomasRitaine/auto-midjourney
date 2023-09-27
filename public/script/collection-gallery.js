a927ab0cdfc3ea5ebf48893d4fc814cffdc6681e
Thomas Ritaine <thomas.ritaine@amaris.com>
2023-09-27 13:43:48 +0200

    feat(Collection): :sparkles: Add download and delete button

--- public/script/collection-gallery.js
+++ public/script/collection-gallery.js
@@ -31,7 +31,60 @@ gallery.querySelectorAll("img").forEach(function (item) {
 });
 window.addEventListener("resize", resizeAll);
 gallery.querySelectorAll(".gallery-item").forEach(function (item) {
-  item.addEventListener("click", function () {
-    item.classList.toggle("full");
+  item.addEventListener("click", function (event) {
+    if (
+      !event.target.classList.contains("btn-download") &&
+      !event.target.classList.contains("btn-delete")
+    ) {
+      item.classList.toggle("full");
+    }
+  });
+});
+
+// Action Buttons
+document.addEventListener("DOMContentLoaded", function () {
+  // Add event listener for download
+  const downloadButtons = document.querySelectorAll(".btn-download");
+  downloadButtons.forEach((btn) => {
+    btn.addEventListener("click", function (event) {
+      const imageUrl = event.target
+        .closest(".content")
+        .querySelector("img").src;
+      const anchor = document.createElement("a");
+      anchor.href = imageUrl;
+      anchor.download = "";
+      anchor.click();
+    });
+  });
+
+  // Add event listener for delete
+  const deleteButtons = document.querySelectorAll(".btn-delete");
+  deleteButtons.forEach((btn) => {
+    btn.addEventListener("click", async function (event) {
+      if (confirm("Are you sure you want to delete this image?")) {
+        const imageUrl = event.target
+          .closest(".content")
+          .querySelector("img").src;
+        const imageID = imageUrl.split("/").pop().split(".")[0]; // Extracting image ID from the URL
+
+        try {
+          const response = await fetch(`/image/${imageID}`, {
+            method: "DELETE",
+          });
+
+          if (response.status === 200) {
+            // Successfully deleted from server, now remove from the DOM
+            event.target.closest(".gallery-item").remove();
+          } else {
+            alert("Error deleting the image. Please try again.");
+          }
+        } catch (error) {
+          console.error("Error sending DELETE request:", error);
+          alert(
+            "Failed to delete the image. Please check your connection and try again."
+          );
+        }
+      }
+    });
   });
 });
