a6f8096aecffbc618c22d70534d7b99386ca3970
Thomas Ritaine <thomas.ritaine@amaris.com>
2023-10-04 11:47:36 +0200

    fix(Prisma): :bug: Fix migration using user.updatedAt before definition

--- prisma/migrations/20231002202757_link_collection_and_generation_info_to_user/migration.sql
+++ prisma/migrations/20231002202757_link_collection_and_generation_info_to_user/migration.sql
@@ -1,6 +1,9 @@
 -- CreateEnum
 CREATE TYPE "GenerationSpeed" AS ENUM ('RELAX', 'FAST');
 
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP;
+
 -- Add a default user of id 00000000-0000-0000-0000-000000000000
 INSERT INTO "User" ("id", "username", "email", "password", "createdAt", "updatedAt") VALUES ('00000000-0000-0000-0000-000000000000', "default-user", "default@default.com", "hash", CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
 
@@ -11,9 +14,6 @@ ADD COLUMN     "userId" TEXT NOT NULL DEFAULT '00000000-0000-0000-0000-000000000
 -- AlterTable
 ALTER TABLE "GenerationInfo" ADD COLUMN     "speed" "GenerationSpeed" NOT NULL DEFAULT 'RELAX',
 ADD COLUMN     "userId" TEXT NOT NULL DEFAULT '00000000-0000-0000-0000-000000000000';
-
--- AlterTable
-ALTER TABLE "User" ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP;
 
 -- AddForeignKey
 ALTER TABLE "GenerationInfo" ADD CONSTRAINT "GenerationInfo_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
