2c2680b4a7155c08f88a3e44fa686bf9bcdd4268
Thomas Ritaine <thomas.ritaine@amaris.com>
2023-11-24 16:04:59 +0100

    feat(Prisma): :card_file_box: Add image isTweeted, and user Twitter OAuth tokens

---
+++ prisma/migrations/20231121213530_add_is_tweeted_to_image/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "Image" ADD COLUMN     "isTweeted" BOOLEAN NOT NULL DEFAULT false;
---
+++ prisma/migrations/20231123175520_add_o_auth1_access_token_and_secret_to_user/migration.sql
@@ -0,0 +1,3 @@
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "twitterOAuth1AccessSecret" TEXT,
+ADD COLUMN     "twitterOAuth1AccessToken" TEXT;
