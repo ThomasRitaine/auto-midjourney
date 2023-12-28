53aefba3516d55c0f5e1a86d9ce71563d22ff55e
Thomas Ritaine <thomas.ritaine@amaris.com>
2023-12-28 23:51:35 +0100

    feat(NFT): :sparkles: Add temporary catch up bot to create NFT of already published images

---
+++ src/services/nft-social-media-bot/catchUpNFTBot.ts
@@ -0,0 +1,52 @@
+import { PrismaClient } from "@prisma/client";
+import generateContent from "../openai/generateContent";
+import createNFT from "../nft/createNFT";
+import sleep from "../../util/sleep";
+
+/**
+ * Processes previously posted images to generate and list NFTs retrospectively.
+ * It fetches all posted images without associated NFTs and sequentially generates content,
+ * creates NFTs, and updates the database. Includes delays to manage load and ensure service stability.
+ */
+export default async (): Promise<void> => {
+  const prisma = new PrismaClient();
+
+  const images = await prisma.image.findMany({
+    where: {
+      isPosted: true,
+      tokenId: null,
+    },
+    include: {
+      generationInfo: {
+        select: {
+          prompt: true,
+        },
+      },
+    },
+  });
+
+  // syncronous for loop with a try catch block and a delay
+  for (const image of images) {
+    try {
+      console.log(`Catch up NFT bot started for image ${image.id}`);
+
+      // Generate the post content with GPT
+      const generatedContent = await generateContent(
+        image.generationInfo.prompt,
+        image.createdAt,
+      );
+      console.log(`Content generated successfully for image ${image.id}`);
+
+      // Create, publish and list the NFT
+      await createNFT(
+        generatedContent.nftName,
+        generatedContent.nftDescription,
+        image,
+      );
+      console.log(`NFT created for image ${image.id}`);
+    } catch (error: any) {
+      console.log(`Error creating NFT for image ${image.id}: ${error.message}`);
+    }
+    await sleep(300000); // Sleeps for 5 minutes
+  }
+};
